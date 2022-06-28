8970d26e80615188ee3f2884afc09249f801414c
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-28 15:58:47 +0300

    establish connection with mongodb w mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const app = express();
 
 // Custum morgan token for better logs
 morgan.token("body", (req) => JSON.stringify(req.body));
+
+// Connect to mongoDB
+logger.info("Connecting to MongoDB..");
+
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => logger.error(error.message));
 
 // Middlewares
 app.use(cors());
--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const { PORT } = process.env;
 
+const { MONGO_URI } = process.env;
+
 module.exports = {
   PORT,
+  MONGO_URI,
 };

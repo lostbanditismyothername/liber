313695015411eb3bd646223ba10e12a688f91c88
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-28 15:50:15 +0300

    add utils & .env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
-PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server up & running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server up & running on port ${config.PORT}`);
 });

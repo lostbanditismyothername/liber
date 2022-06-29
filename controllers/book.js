e574fc3f0d6b792a2efcb44bdb78b6079f93cd53
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-29 15:04:46 +0300

    add delete & put methods

--- controllers/book.js
+++ controllers/book.js
@@ -1,24 +1,50 @@
 const Book = require("../models/book");
 
-// Errors are thrown with express-async-errors package, no need for catch
-const getBooks = async (req, res) => {
+// HTTP errors are being handled with express-async-errors
+// therefore no need to write catch
+
+// Get all books
+const getAllBooks = async (req, res) => {
   const users = await Book.find({});
-  res.status(200).json(users);
+  res.status(200).json({ users: users });
 };
 
+// Get a book by id
+const getBook = async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  res.json(book);
+};
+
+// Insert a new book
 const addBook = async (req, res) => {
   const { body } = req;
   const { title, author, language, likes } = body;
-
   const newBookObj = new Book({
     title,
     author,
     language,
     likes,
   });
-  const newBook = await newBookObj.save();
 
+  const newBook = await newBookObj.save();
   res.status(201).json(newBook);
 };
 
-module.exports = { getBooks, addBook };
+// Update a book
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  const newBookObj = req.body;
+
+  const updatedBook = await Book.findByIdAndUpdate(id, newBookObj, {
+    new: true,
+  });
+  res.json(updatedBook);
+};
+
+// Delete book
+const deleteBook = async (req, res) => {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  res.status(204);
+};
+
+module.exports = { getAllBooks, addBook, updateBook, getBook, deleteBook };
--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,10 @@ const bookController = require("../controllers/book");
 
 const bookRouter = express.Router();
 
-bookRouter.get("/books", bookController.getBooks);
+bookRouter.get("/books", bookController.getAllBooks);
+bookRouter.get("/books/:id", bookController.getBook);
 bookRouter.post("/books", bookController.addBook);
+bookRouter.put("/books/:id", bookController.updateBook);
+bookRouter.delete("/books/:id", bookController.deleteBook);
 
 module.exports = bookRouter;
